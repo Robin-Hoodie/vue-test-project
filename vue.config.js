2bc867d0b24cd39a1a0cb413f860196c6484a902
RobinHoodie <robin@oreon.io>
2020-07-27 13:48:56 +0200

    Set up test project for issue with 'build --target lib'

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerMode: "static"
+      })
+    ]
+  }
+};
